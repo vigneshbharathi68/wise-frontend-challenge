81876bb2ba04e98225c6392cbae86aaa7aa6cb75
vigneshbharathi68 <vigneshbharathi68@hotmail.com>
2021-12-07 14:20:28 +0530

    Base setup of the project added

---
+++ script.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            data: "data Instance",
+            active: 'buy'
+        }
+    },
+    methods: {
+        
+    }
+}).mount("#app")
